43750dfb9f0b323923d626ca84c6681101b27ea7
ea1a87 <163140@autistici.org>
2020-08-30 18:35:21 +1000

    РЕализованы функции find/3 и line_eq/1

--- src/line.erl
+++ src/line.erl
@@ -1,6 +1,6 @@
 -module(line).
 -include_lib("eunit/include/eunit.hrl").
--export([new/2, len/1]).
+-export([new/2, len/1, find/3, line_eq/1]).
 -author("ea1a87").
 
 
@@ -35,3 +35,33 @@ len(Line) ->
 	Dy	=	Y2 - Y1,
 	math:sqrt(math:pow(Dx, 2) + math:pow(Dy, 2)).
 len_test()->?_assert( {line, {point,10,96}, {point,-22,80} } =:= math:sqrt(1280) ).
+
+%% @doc Рассчитывает неизвестную координату по известной на заданной прямой
+-spec find('x?' | 'y?', line(), number() )	->	number().
+find('x?', Line, Y)	->
+	{line_eq,	K,	B}	=	line_eq(Line),
+	(Y - B) / K;
+find('y?', Line, X)	->
+	{line_eq,	K,	B}	=	line_eq(Line),
+	K * X + B.
+find_test_() -> [
+	?_assert( find( 'x?', {line, {point,0,0}, {point,10,10}}, 7 )  =:= 7 ),
+	?_assert( find( 'y?', {line, {point,0,0}, {point,10,10}}, 3 )  =:= 3 )
+].
+
+%% @doc Возвращает коэффициенты уравнения прямой заданной двумя точками.
+-spec line_eq(Line) -> {'line_eq', number(), number()}.
+line_eq(Line) ->
+	{line	, A1, A2}	=	Line,
+	{point,	X1,	Y1}	=	A1,
+	{point,	X2,	Y2}	=	A2,
+	Dx	=	X2 - X1			,
+	Dy	=	Y2 - Y1			,
+	K		= Dy/Dx				,
+	B		=	Y1 - (K*X1)	,
+	{line_eq,	K, B}.
+line_eq_test() -> ?_assert( line_eq({ line, {point,10,20}, {point,20,50} }) =:= {line_eq, 3, -10} ). 
+
+
+
+
